1f4c01f2ab647af0539031b3d44cb319c907ef5c
Marcos Vinícius <rkatchau@gmail.com>
2023-09-15 02:24:43 -0300

    add: conexão com banco de dados

--- rinha-de-backend/src/app.module.ts
+++ rinha-de-backend/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PessoasController } from './pessoas/pessoas.controller';
 import { PessoasService } from './pessoas/pessoas.service';
+import { PessoasModule } from './pessoas/pessoas.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
+    }),
+    PessoasModule,
+  ],
   controllers: [PessoasController],
   providers: [PessoasService],
 })
