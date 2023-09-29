79662eaeafa3174cf461916150555bf28489f863
Marcos Vinícius <rkatchau@gmail.com>
2023-09-29 15:25:50 -0300

    update: propriedades na documentação

--- rinha-de-backend/src/pessoa/dto/create-pessoa.dto.ts
+++ rinha-de-backend/src/pessoa/dto/create-pessoa.dto.ts
@@ -1,17 +1,38 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsArray, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreatePessoaDto {
+  @ApiProperty({
+    type: String,
+    default: 'Apelido de Exemplo',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   apelido: string;
 
+  @ApiProperty({
+    type: String,
+    default: 'Nome de Exemplo',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   nome: string;
 
+  @ApiProperty({
+    type: String,
+    default: '1996-05-21',
+    required: true,
+  })
   @IsNotEmpty()
   nascimento: string;
 
+  @ApiProperty({
+    type: [String],
+    default: ['C', 'SQL', 'Node'],
+    required: false,
+  })
   @IsArray()
   stack: string[] | null;
 }
--- rinha-de-backend/src/pessoa/pessoa.controller.ts
+++ rinha-de-backend/src/pessoa/pessoa.controller.ts
@@ -1,27 +1,60 @@
 import { Controller, Get, Post, Body, Query, Param } from '@nestjs/common';
 import { PessoaService } from './pessoa.service';
 import { CreatePessoaDto } from './dto/create-pessoa.dto';
+import { ApiBody, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Pessoas')
 @Controller('pessoa')
 export class PessoaController {
   constructor(private readonly pessoaService: PessoaService) {}
 
   @Post()
+  @ApiBody({
+    type: [CreatePessoaDto],
+    required: true,
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'A pessoa foi criada com sucesso',
+  })
+  @ApiResponse({
+    status: 422,
+    description: 'Requisição inválida',
+  })
   async create(@Body() createPessoaDto: CreatePessoaDto) {
     return await this.pessoaService.create(createPessoaDto);
   }
 
   @Get('/:id')
+  @ApiParam({
+    type: String,
+    name: 'id',
+    description: 'Identificador da pessoa',
+    required: true,
+  })
+  @ApiResponse({ status: 200, description: 'Ok' })
+  @ApiResponse({
+    status: 404,
+    description: 'Não há nenhuma pessoa com esse ID',
+  })
   async findOne(@Param('id') id: string) {
     return await this.pessoaService.findOne(id);
   }
 
   @Get()
+  @ApiParam({
+    type: String,
+    name: 't',
+    description: 'Termo procurado',
+    required: true,
+  })
+  @ApiResponse({ status: 200, description: 'Ok' })
   async search(@Query('t') t: string) {
     return await this.pessoaService.search(t);
   }
 
   @Get('/contagem-pessoas')
+  @ApiResponse({ status: 200, description: 'Ok' })
   async count() {
     return await this.pessoaService.count();
   }
